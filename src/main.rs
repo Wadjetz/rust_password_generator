75bb61a8146adb6e1ef44cc9b470a3bb03c61e04
Egor Berezovskiy <egor.neon@gmail.com>
2017-12-08 09:23:26 +0100

    Clean

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ extern crate rand;
 use std::fs::File;
 use std::io::BufReader;
 use std::io::BufRead;
-use std::path::{Path, PathBuf};
+use std::path::Path;
 
 use rand::Rng;
 
@@ -12,19 +12,15 @@ fn main() {
     let file = File::open(path).unwrap();
     let reader = BufReader::new(file);
 
-    
-
     let lines = reader.lines()
         .filter(|line| line.is_ok())
         .map(|line| line.unwrap().to_lowercase())
         .collect::<Vec<String>>();
-
-    let mut password = String::new();
 
     let password = (0..4).map(|_| {
         let r: usize = rand::thread_rng().gen_range(0, lines.len());
         lines[r].clone()
     }).collect::<Vec<String>>();
 
-    println!("{:?}", password.join("-"));
+    println!("{}", password.join("-"));
 }
